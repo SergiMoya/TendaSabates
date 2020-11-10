78a734a5187902131603f06c9ca275639aad79f8
SergiMoya <48238996+SergiMoya@users.noreply.github.com>
2020-11-10 12:40:29 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
 </head>
 
 <body>
@@ -21,7 +23,7 @@
         die("Connection failed: " . $conn->connect_error);
     }
 
-    $sql = "SELECT id, firstname, lastname FROM MyGuests";
+    $sql = "SELECT * FROM Producte";
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
