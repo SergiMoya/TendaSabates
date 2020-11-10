2da7dbc8a75b54ea985312ea6eb3d5c93475c51e
SergiMoya <48238996+SergiMoya@users.noreply.github.com>
2020-11-10 12:50:39 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,52 +1,58 @@
+<?php
+
+        $servername = "0.0.0.0";
+        $username = "perez";
+        $password = "moya1234";
+        $dbname = "TendaBD";
+
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        //make query
+        $sql = "Select id, model, preu from Producte";
+        $result = $conn->query($sql);
+        $conn->close();
+
+    ?>
+
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
+    <title>Hat Shop</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
-
 <body>
-    <?php
-    $servername = "0.0.0.0";
-    $username = "perez";
-    $password = "moya1234";
-    $dbname = "TendaBD";
+    
 
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
+    <div class="container">
+        <div class="row">
 
-    $sql = "SELECT * FROM Producte";
-    $result = $conn->query($sql);
+            <?php 
+                if($result->num_rows > 0){
+                    while($row = $result->fetch_assoc()){
+                        $id = $row["id"];
+                        $model = $row["model"];
+                        $preu = $row["preu"];
+                        
+                       
+            ?>
 
-    if ($result->num_rows > 0) {
-        // output data of each row
-        while ($row = $result->fetch_assoc()) {
-    ?>
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-3">
-                        <img src="" alt="" class="img-fluid">
-                        <h4 class="text-center"><?php echo $row['model'] ?></h4>
-                        <h6 class="text-center"><?php echo $row['preu'] ?></h6>
-                        <a href="fitxa.php?id=<?php echo $row['id'] ?>"></a>
-                    </div>
-                </div>
-            </div>
-    <?php
-        }
-    } else {
-        echo "0 results";
-    }
-    $conn->close();
-    ?>
+            <div class="col-sm-4"><?php echo $model . " " . $preu . "€ "  ."<a href='fitxa.php?id=$id'>Mes informacio</a>" . "<br><br>"; ?></div>
+
+            <?php
+                    }
+                }else{
+                    echo "0 results";
+                }
+            ?>
+
+        </div>
+    </div>
+
 </body>
-
 </html>
