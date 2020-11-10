f06ce421d7f296ab0b9a3a56a3543f750f4756f1
SergiMoya <48238996+SergiMoya@users.noreply.github.com>
2020-11-10 12:21:52 +0100

    Update Fitxa.php

--- public/Fitxa.php
+++ public/Fitxa.php
@@ -1,3 +1,30 @@
+<?php
+
+$codi = $_GET['id'];
+$servername = "0.0.0.0";
+$username = "perez";
+$password = "moya1234";
+$dbname = "TendaBD";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT id, model, preu FROM Producte";
+$result = $conn->query($sql);
+
+$row = $result->fetch_assoc();
+$model = $row['model'];
+$preu = $row['preu'];
+
+$conn->close();
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,11 +36,11 @@
 <body>
 <div class="container">
     <div class="row">
-        <div class="col-sm-6"><img src="" alt="" class="img-fluid"></div>
+        <div class="col-sm-6"><img src="imatges/<?php echo $codi; ?>" alt="" class="img-fluid"></div>
         <div class="col-sm-6">
             <div class="row">
-                <div class="col-sm-11"><h2>MODEL SABATES</h2></div>
-                <div class="col-sm-11"><h4>PREU</h4></div>
+                <div class="col-sm-11"><h2><?php echo $model; ?></h2></div>
+                <div class="col-sm-11"><h4><?php echo $preu; ?></h4></div>
                 <div class="col-sm-11"><button class="btn btn-primary">Afegir</button></div>
             </div>
         </div>
