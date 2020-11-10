88fa0857cbcb733d2a833b4da9e439e0879f5c32
SergiMoya <48238996+SergiMoya@users.noreply.github.com>
2020-11-10 12:24:07 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row col-sm-3">
+            <img src="" alt="" class="img-fluid">
+            <h4 class="text-center">NOM PRODUCTE</h4>
+        </div>
+    </div>
+</body>
+</html>
