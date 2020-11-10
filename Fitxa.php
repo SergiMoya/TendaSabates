dc74b6acf92545dea9cbe45489b860d66a31607e
SergiMoya <48238996+SergiMoya@users.noreply.github.com>
2020-11-10 10:17:33 +0100

    Update Fitxa.php

--- Fitxa.php
+++ Fitxa.php
@@ -6,6 +6,31 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Hola</h1>
+<?php
+$servername = "0.0.0.0";
+$username = "perez";
+$password = "moya1234";
+$dbname = "TendaBD";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM Producte";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["id"]. " - model: " . $row["model"]. " " . $row["preu"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
 </body>
 </html>
