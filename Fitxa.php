af7ba943aa989a3391d2a1aeab732b91f85b5129
SergiMoya <48238996+SergiMoya@users.noreply.github.com>
2020-11-10 11:50:59 +0100

    Update Fitxa.php

--- Fitxa.php
+++ Fitxa.php
@@ -21,26 +21,29 @@ if ($conn->connect_error) {
 }
 
 $sql = "SELECT * FROM Producte";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-  // output data of each row
-  $product_html = '';
-  while($row = $result->fetch_assoc()) {
-    
-    while( $product = mysqli_fetch_assoc($product_results) ) {
-        $product_html .= '<div class="col-md-3 product">';
-        $product_html .= '<h4>'.$product["model"].'</h4>';
-        $product_html .= '<h4>Price: $'.$product["preu"].'</h4>';
-        $product_html .= '</div>';
-        }
-        $product_html .= '<div class="clear_both"></div></div>';
-        }
-  }
- else {
-  echo "0 results";
+$category_sql = "SELECT id, name FROM categories LIMIT 10";
+$resultset = mysqli_query($conn, $category_sql) or die("database error:". mysqli_error($conn));
+$active_class = 0 ;
+$category_html = '';
+$product_html = '';
+while( $category = mysqli_fetch_assoc($resultset) ) {
+$current_tab = "";
+$current_content = "";
+if(!$active_class) {
+$active_class = 1;
+$current_tab = 'active';
+$current_content = 'in active';
 }
-$conn->close();
+while( $product = mysqli_fetch_assoc($product_results) ) {
+    $product_html .= '<div class="col-md-3 product">';
+    
+    $product_html .= '<h4>'.$product["model"].'</h4>';
+    $product_html .= '<h4>Price: $'.$product["preu"].'</h4>';
+    $product_html .= '</div>';
+    }
+    $product_html .= '<div class="clear_both"></div></div>';
+    }
+
 ?>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
