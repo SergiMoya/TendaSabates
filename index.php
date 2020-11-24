8d88fa9315da8cf1ee1617fd5b6708627fb8b9e5
SergiMoya <48238996+SergiMoya@users.noreply.github.com>
2020-11-24 12:15:25 +0100

    Update index.php

--- index.php
+++ index.php
@@ -20,29 +20,29 @@ $conn->close();
 <?php
 $lang = "es";
 
-if(isset($_GET["lang"]) && $_GET["lang"]!=""){
-if($_GET["lang"]=="en" || $_GET["lang"]=="es"){
-	$lang = $_GET["lang"];
-}
+if (isset($_GET["lang"]) && $_GET["lang"] != "") {
+    if ($_GET["lang"] == "en" || $_GET["lang"] == "es") {
+        $lang = $_GET["lang"];
+    }
 }
 
 $frases = array(
-	"es"=>array(
-		"inici"=>"Inici",
-		"carrito"=>"Carreto",
-		"contacte"=>"Contacte",
-        "afegir"=>"Afegir Productes",
-        "idioma"=>"Idioma"
-		),
-	"en"=>array(
-		"inici"=>"Home",
-		"carrito"=>"Cart",
-		"contacte"=>"Contact",
-        "afegir"=>"Add Product",
-        "idioma"=>"Language"
-		)
+    "es" => array(
+        "inici" => "Inici",
+        "carrito" => "Carreto",
+        "contacte" => "Contacte",
+        "afegir" => "Afegir Productes",
+        "idioma" => "Idioma"
+    ),
+    "en" => array(
+        "inici" => "Home",
+        "carrito" => "Cart",
+        "contacte" => "Contact",
+        "afegir" => "Add Product",
+        "idioma" => "Language"
+    )
 
-	);
+);
 ?>
 
 <!DOCTYPE html>
@@ -52,7 +52,8 @@ $frases = array(
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tenda de Sabates</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css">
 
 </head>
 
@@ -80,12 +81,12 @@ $frases = array(
                         <a class="nav-link efecte" href="AfegirProductes.php"><?php echo $frases[$lang]["afegir"]; ?></a>
                     </li>
                     <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#"><?php echo $frases[$lang]["idioma"]; ?></a>
-                            <div class="dropdown-menu">
-                                <a class="dropdown-item" href="index.php?lang=es">Español</a>
-                                <a class="dropdown-item" href="index.php?lang=en">Ingles</a>
-                            </div>
-                        </li>
+                        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#"><?php echo $frases[$lang]["idioma"]; ?></a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="index.php?lang=es">Español</a>
+                            <a class="dropdown-item" href="index.php?lang=en">Ingles</a>
+                        </div>
+                    </li>
                 </ul>
             </div>
 
@@ -120,7 +121,9 @@ $frases = array(
 
         </div>
     </div>
-
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 
 </html>
