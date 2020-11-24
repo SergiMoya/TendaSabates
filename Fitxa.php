4d9d01bee27131ac7f770573fde3c9a65fd39756
SergiMoya <48238996+SergiMoya@users.noreply.github.com>
2020-11-24 13:24:20 +0100

    Update Fitxa.php

--- Fitxa.php
+++ Fitxa.php
@@ -89,7 +89,8 @@ include_once "TriarIdioma.php";
                     <div class="col-sm-11">
                         <h8><?php echo $descripcio; ?></h8>
                     </div>
-                    <div class="col-sm-11"> "<a href='agregar.php?id=<?php echo $id ?>><?php echo $frases[$lang]["afegir"]; ?></a> </div>
+                    <div class="col-sm-11"> 
+                    <a href='agregar.php?id=<?php echo $id; ?>' class="btn btn-primary"><?php echo $frases[$lang]["agregar"]; ?></a>
                 </div>
             </div>
         </div>
