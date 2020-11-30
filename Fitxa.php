b44afc483a5e0651ec6ec6d23d945e1f6273de75
SergiMoya <48238996+SergiMoya@users.noreply.github.com>
2020-11-30 20:25:31 +0100

    Update Fitxa.php

--- Fitxa.php
+++ Fitxa.php
@@ -1,5 +1,19 @@
 <?php
 session_start();
+// Set Language variable
+if (isset($_GET['lang']) && !empty($_GET['lang'])) {
+    $_SESSION['lang'] = $_GET['lang'];
+
+    if (isset($_SESSION['lang']) && $_SESSION['lang'] != $_GET['lang']) {
+        echo "<script type='text/javascript'> location.reload(); </script>";
+    }
+}
+// Include Language file
+if (isset($_SESSION['lang'])) {
+    include "lang_" . $_SESSION['lang'] . ".php";
+} else {
+    include "lang_es.php";
+}
 $codi = $_GET['id'];
 $servername = "0.0.0.0";
 $username = "perez";
@@ -23,20 +37,7 @@ $preu = $row['preu'];
 $descripcio = $row['descripcio'];
 $conn->close();
 
-// Set Language variable
-if (isset($_GET['lang']) && !empty($_GET['lang'])) {
-    $_SESSION['lang'] = $_GET['lang'];
 
-    if (isset($_SESSION['lang']) && $_SESSION['lang'] != $_GET['lang']) {
-        echo "<script type='text/javascript'> location.reload(); </script>";
-    }
-}
-// Include Language file
-if (isset($_SESSION['lang'])) {
-    include "lang_" . $_SESSION['lang'] . ".php";
-} else {
-    include "lang_es.php";
-}
 
 ?>
 
